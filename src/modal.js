754c77a6f50058dfd997bdfb1a96241ed53a2590
github username <github email address>
2020-07-08 17:24:24 -0400

    setTimeout for modal popup and delete on modal open, create game on modal close

--- src/modal.js
+++ src/modal.js
@@ -3,14 +3,17 @@ const newGameButton = document.querySelector("#new-game-button")
 
 newGameButton.addEventListener("click", (e) => {  
     modal.style.display = "none"
-    const gameId = e.target.dataset.id      
-    deleteRound(gameId)
+    createNewRound(currentRoomCode)
 })
 
 function gameOver(gameObj, winner) {
+    window.setTimeout(displayModal, 300, gameObj, winner);    
+}
+
+function displayModal(gameObj, winner){
+    // once the game is over (and modal pops up), the game is destroyed
+    deleteRound(gameObj.id)
     modal.style.display = "block"
     const winnerTag = document.querySelector("#winner-tag")
     winnerTag.textContent = `${winner} Team Wins`
-
-    newGameButton.dataset.id = gameObj.id  
 }
