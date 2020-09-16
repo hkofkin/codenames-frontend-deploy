d668d4b2b07a519cbe0328ea178a13ee458be203
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-09-16 15:33:28 -0400

    remove some code that the websockets take care of now

--- src/fetch.js
+++ src/fetch.js
@@ -62,15 +62,12 @@ function createNewRound(currentRoomCode) {
       },
       body: JSON.stringify({room_code: currentRoomCode}),
   })
-      .then(response => response.json())
-      .then(newGameData => {
+  // don't need the .thens because websockets are taking care of the display changes
+      // .then(response => response.json())
+      // .then(newGameData => {
           // displayGame(newGameData)
-          // wordsLeftNumber.textContent = newGameData.orange_words_left
-          teamColorTurn.textContent = "orange"
-          spymasterViewButton.textContent = "SPYMASTER VIEW: OFF"
+          // teamColorTurn.textContent = "orange"
+          // spymasterViewButton.textContent = "SPYMASTER VIEW: OFF"
           // createGameRoomWebsocketConnection(currentRoomCode)
-          
-          // console.log("creating new round--------------")
-          // createGameRoomWebsocketConnection(currentRoomCode)
-      })
+      // })
 }
